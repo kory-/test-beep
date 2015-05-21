81508878ec0419c03717da750791a2ff741e6728
Keita Shimono <shimono@socialgrid.co.jp>
2015-05-21 12:57:38 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+ZGN(function()
+{
+  // 18番ピンで動作させます
+  var Pin = '18';
+
+  // TerminalのGPIOインスタンスを取得します
+  var gpio = ZGN.term('1').gpio;
+
+  // 調光の初期値を0に設定します
+  var level = 0;
+
+  // 指定ピンをPWMに設定
+  gpio.pinMode(Pin, ZGN.PWM);
+
+  // upボタンをクリック
+  $(document).on('click', '#up', function() {
+    level = level >= 10 ? 10 : level + 1; // levelを1上げる
+    gpio.pwmWrite(Pin, level / 10); // デューティー比の設定
+  });
+
+  // dwonボタンをクリック
+  $(document).on('click', '#down', function() {
+    level = level <= 0 ? 0 : level - 1; // levelを1下げる
+    gpio.pwmWrite(Pin, level / 10); // デューティー比の設定
+  });
+});
