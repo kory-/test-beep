139a415aa66f177dfe7cd065995d5340b0a6f592
Keita Shimono <shimono@socialgrid.co.jp>
2015-05-26 13:42:13 +0900

    change

--- app.js
+++ app.js
@@ -13,20 +13,36 @@ ZGN(function()
   // 指定ピンをPWMに設定
   gpio.pinMode(Pin, ZGN.PWM);
 
-  // upボタンをクリック
-  $(document).on('click', '#up', function() {
-    level = level + 100; // levelを1上げる
+  setInterval("beep()",100);
+
+  function beep(){
+    level = level + 1;
     gpio.pwmWrite(Pin, level / dute); // デューティー比の設定
 
     $('#pwm').text(level/dute);
-  });
+  }
 
-  // dwonボタンをクリック
-  $(document).on('click', '#down', function() {
-    level = level <= 0 ? 0 : level - 100; // levelを1下げる
-    gpio.pwmWrite(Pin, level / dute); // デューティー比の設定
+  // // upボタンをクリック
+  // $(document).on('click', '#up', function() {
+  //   level = level + 100; // levelを1上げる
+  //   gpio.pwmWrite(Pin, level / dute); // デューティー比の設定
 
-    $('#pwm').text(level/dute);
-  });
+  //   $('#pwm').text(level/dute);
+  // });
 
+  // // dwonボタンをクリック
+  // $(document).on('click', '#down', function() {
+  //   level = level <= 0 ? 0 : level - 100; // levelを1下げる
+  //   gpio.pwmWrite(Pin, level / dute); // デューティー比の設定
+
+  //   $('#pwm').text(level/dute);
+  // });
+
+  // // dwonボタンをクリック
+  // $(document).on('click', '#stop', function() {
+  //   level = 0;
+  //   gpio.pwmWrite(Pin, level); // デューティー比の設定
+
+  //   $('#pwm').text(0);
+  // });
 });
